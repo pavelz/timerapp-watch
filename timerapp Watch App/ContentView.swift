82886dfa4c71dbd8fc5d6474d3890bbc9e9a0237
Pavel Zaitsev <pavel@arslogi.ca>
2023-08-17 21:20:51 -0400

    Initial Commit

---
+++ timerapp Watch App/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  timerapp Watch App
+//
+//  Created by Pavel Zaitsev on 2023-08-17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ timerapp Watch App/timerappApp.swift
@@ -0,0 +1,17 @@
+//
+//  timerappApp.swift
+//  timerapp Watch App
+//
+//  Created by Pavel Zaitsev on 2023-08-17.
+//
+
+import SwiftUI
+
+@main
+struct timerapp_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
