cf3d44671d2955910bcbec2241b3a198ce23a4f9
Pavel Zaitsev <pavel@arslogi.ca>
2023-08-19 15:22:10 -0400

    ey

--- timerapp Watch App/ContentView.swift
+++ timerapp Watch App/ContentView.swift
@@ -6,14 +6,23 @@
 //
 
 import SwiftUI
+import AVFoundation
 
 struct ContentView: View {
     var body: some View {
         VStack {
+            Button(action: {
+//                Timer.scheduledTimer(withTimeInterval: 1.0, repeats: false){
+                    WKInterfaceDevice.current().play(.failure)
+//                }
+            }
+            ){
+                Text("Timer")
+            }
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("🤡Boop , world!")
         }
         .padding()
     }
